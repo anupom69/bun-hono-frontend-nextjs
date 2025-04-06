c26cbba4146a136fb1146afce0ff32b52c2c27e2
trinity <royanupom69@gmail.com>
2025-04-06 18:29:34 +0600

    Update UpdateMessageForm.tsx

--- src/components/UpdateMessageForm.tsx
+++ src/components/UpdateMessageForm.tsx
@@ -102,7 +102,7 @@ export default function MessageUpdateForm({
           </Label>
           <Input
             type="number"
-            step={0.01}
+            step={0.001}
             min={0}
             name="sendAfter"
             defaultValue={message.sendAfter}
