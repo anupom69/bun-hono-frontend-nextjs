e08b494f212e49db75007f58efb632eedf8d35ca
trinity <royanupom69@gmail.com>
2025-04-06 15:52:56 +0600

    min added to sendAfter

--- src/components/UpdateMessageForm.tsx
+++ src/components/UpdateMessageForm.tsx
@@ -102,6 +102,7 @@ export default function MessageUpdateForm({
           </Label>
           <Input
             type="number"
+            min={0}
             name="sendAfter"
             defaultValue={message.sendAfter}
           />
